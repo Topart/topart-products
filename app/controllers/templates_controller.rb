67ffefb73a45f6518244e02e794ee53d3c90a80a
Axel Campailla <axelcampailla@hotmail.com>
2012-10-05 10:13:39 -0700

    layered products - v1

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -8,7 +8,7 @@ class TemplatesController < ApplicationController
 	def index
  
 		# Load the source Excel file, with all the special products info
-		source = Excelx.new("http://beta.topart.com/csv/special.xlsx")
+		source = Excelx.new("http://beta.topart.com/csv/special_products.xlsx")
 		source.default_sheet = source.sheets.first
 		
 		# Load the Magento template, which is in Open Office format
@@ -17,92 +17,58 @@ class TemplatesController < ApplicationController
 		
 		# Fill every line in the template file up with
 		# the right value taken from the source input file
-		2.upto(989) do |line|
 		
-			template.set(line, 'BV', "#{source.cell(line,'A')}")
-			template.set(line, 'A', "#{source.cell(line,'B')}")
-			template.set(line, 'AR', "#{source.cell(line,'B')}")
-			template.set(line, 'CM', "#{source.cell(line,'C')}")
-			template.set(line, 'CK', "#{source.cell(line,'D')}")
-			template.set(line, 'AT', "#{source.cell(line,'E')}")
-			template.set(line, 'CL', "#{source.cell(line,'F')}")
-			template.set(line, 'AU', "#{source.cell(line,'G')}")
-			template.set(line, 'CI', "#{source.cell(line,'H')}")
-			template.set(line, 'L', "#{source.cell(line,'I')}")
-			template.set(line, 'M', "#{source.cell(line,'J')}")
-			# we got until "artist_last_name"
+		#2.upto(989) do |line|
+		@line = 2
+		while @line < source.last_row
+		
+			template.set(@line, 'A', "#{source.cell(@line,'B')}")
 			
-			template.set(line, 'BG', "#{source.cell(line,'K')}")
-			template.set(line, 'CQ', "#{source.cell(line,'K')}")
-			template.set(line, 'AB', "#{source.cell(line,'K')}")
-			template.set(line, 'CC', "#{source.cell(line,'K')}")
+			template.set(@line, 'C', "Topart - Special Products")
+			template.set(@line, 'D', "simple")
+			template.set(@line, 'E', "Collections/Oscar Night")
+			template.set(@line, 'F', "Root Category")
+			template.set(@line, 'G', "base")
 			
-			template.set(line, 'BU', "#{source.cell(line,'L')}")
-			template.set(line, 'BP', "#{source.cell(line,'M')}")
-			template.set(line, 'BQ', "#{source.cell(line,'N')}")
-			template.set(line, 'AP', "#{source.cell(line,'O')}")
-			template.set(line, 'AQ', "#{source.cell(line,'P')}")
-			template.set(line, 'AN', "#{source.cell(line,'Q')}")
-			template.set(line, 'CB', "#{source.cell(line,'Q')}")
-			template.set(line, 'H', "#{source.cell(line,'S')}")
-			# we got until "alt_size_1"
+			template.set(@line, 'H', "#{source.cell(@line,'S')}")
+			template.set(@line, 'I', "#{source.cell(@line,'T')}")
+			template.set(@line, 'J', "#{source.cell(@line,'U')}")
+			template.set(@line, 'K', "#{source.cell(@line,'V')}")
 			
-			template.set(line, 'I', "#{source.cell(line,'T')}")
-			template.set(line, 'J', "#{source.cell(line,'U')}")
-			template.set(line, 'K', "#{source.cell(line,'V')}")
-			template.set(line, 'BM', "#{source.cell(line,'W')}")
-			template.set(line, 'BY', "#{source.cell(line,'X')}")
-			template.set(line, 'AE', "#{source.cell(line,'Y')}")
-			template.set(line, 'CD', "#{source.cell(line,'Z')}")
-			template.set(line, 'AH', "#{source.cell(line,'AA')}")
-			template.set(line, 'AX', "#{source.cell(line,'AB')}")
-			template.set(line, 'AM', "#{source.cell(line,'AC')}")
-			# we got until "ignore_discount"
+			template.set(@line, 'L', "#{source.cell(@line,'I')}")
+			template.set(@line, 'M', "#{source.cell(@line,'J')}")
 			
-			template.set(line, 'T', "#{source.cell(line,'AD')}")
-			template.set(line, 'BN', "#{source.cell(line,'AE')}")
-			template.set(line, 'AI', "#{source.cell(line,'AF')}")
-			template.set(line, 'CA', "#{source.cell(line,'AG')}")
-			template.set(line, 'O', "#{source.cell(line,'AH')}")
-			template.set(line, 'BH', "#{source.cell(line,'AI')}")
-			template.set(line, 'BX', "#{source.cell(line,'AJ')}")
-			template.set(line, 'AS', "#{source.cell(line,'AK')}")
-			template.set(line, 'Y', "#{source.cell(line,'AL')}")
-			template.set(line, 'S', "#{source.cell(line,'AM')}")
-			# we got until "created_by"
+			# Color: Look into "keywords" i.e. "AK", and search for colors...
+			# ...and add each color to the same column but one @line below
+			@color_set = Array["red", "orange", "yellow", "green", "blue", "purple", "pink", "brown", "black/white", "black", "white"]
+			@color_count = 0;
+			0.upto(@color_set.length) do |n|
+				if "#{source.cell(@line,'AK')}".include? "#{@color_set[n]}"
+					#@line = @line + @color_count
+					template.set(@line + @color_count, 'N', "#{@color_set[n]}")
+					@color_count = @color_count + 1
+				end
+			end
+			
+			# Color Tone
+			template.set(@line, 'O', "#{source.cell(@line,'AH')}")
+			
+			template.set(@line, 'AL', "0")
+			template.set(@line, 'BE', "Use config")
+			template.set(@line, 'BF', "Use config")
+			template.set(@line, 'BL', "Block after Info Column")
+			template.set(@line, 'CJ', "1")
+			
+			template.set(@line, 'BZ', "1")
+			template.set(@line, 'CV', "1")
+			template.set(@line, 'CU', "4")
+			template.set(@line, 'CN', "0")
 	  
-			template.set(line, 'AA', "#{source.cell(line,'AN')}")
-			template.set(line, 'BC', "#{source.cell(line,'AO')}")
-			template.set(line, 'Z', "#{source.cell(line,'AP')}")
-			template.set(line, 'AG', "#{source.cell(line,'AQ')}")
-			template.set(line, 'AC', "#{source.cell(line,'AR')}")
-			template.set(line, 'BR', "#{source.cell(line,'AS')}")
-			template.set(line, 'BT', "#{source.cell(line,'AT')}")
-			template.set(line, 'BS', "#{source.cell(line,'AU')}")
-			template.set(line, 'BK', "#{source.cell(line,'AV')}")
-			template.set(line, 'CF', "#{source.cell(line,'AW')}")
-			template.set(line, 'AD', "#{source.cell(line,'AX')}")
-			# we got until "do_not_display"
-			
-			template.set(line, 'C', "Topart - Special Products")
-			template.set(line, 'D', "simple")
-			template.set(line, 'E', "Collections/Oscar Night")
-			template.set(line, 'F', "Root Category")
-			template.set(line, 'G', "base")
-			template.set(line, 'AL', "0")
-			template.set(line, 'BE', "Use config")
-			template.set(line, 'BF', "Use config")
-			template.set(line, 'BL', "Block after Info Column")
-			template.set(line, 'CJ', "1")
-			
-			template.set(line, 'BZ', "1")
-			template.set(line, 'CV', "1")
-			template.set(line, 'CU', "4")
-			template.set(line, 'CN', "0")
-	  
+			@line = @line + @color_count
+			@line = @line + 1
 		end
 		
-		template.to_csv("filled_template.csv")
+		template.to_csv("filled_layered_template.csv")
  
 		respond_to do |format|
 			format.html # index.html.erb
